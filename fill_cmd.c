8879ab5b88e237eff5b0053a74f1933343bf6736
benali-chadi <benalioulhajchadi@gmail.com>
2021-01-29 18:47:05 +0100

    norminette stuff

--- fill_cmd.c
+++ fill_cmd.c
@@ -6,11 +6,49 @@
 /*   By: cbenali- <cbenali-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 15:08:59 by cbenali-          #+#    #+#             */
-/*   Updated: 2021/01/28 15:47:26 by cbenali-         ###   ########.fr       */
+/*   Updated: 2021/01/29 18:45:49 by cbenali-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_shell.h"
+
+char	*clean_command(char *command)
+{
+	char	*str;
+	int		quote;
+	int		i;
+	int		k;
+
+	i = 0;
+	k = 0;
+	quote = 0;
+	str = m_malloc(ft_strlen(command));
+	while (command[i])
+	{
+		if (command[i] == '\'')
+		{
+			if (quote == 0)
+				quote = 1;
+			else if (quote == 1)
+				quote = 0;
+			else
+				str[k++] = command[i];
+		}
+		else if (command[i] == '"')
+		{
+			if (quote == 0)
+				quote = 2;
+			else if (quote == 2)
+				quote = 0;
+			else
+				str[k++] = command[i];
+		}
+		else
+			str[k++] = command[i];
+		i++;
+	}
+	return (str);
+}
 
 int		fill_cmd_helper(char **split)
 {
@@ -29,7 +67,7 @@ int		fill_cmd_helper(char **split)
 		i++;
 	}
 	g_cmd->command = m_malloc(ft_strlen(split[i]) + 1);
-	ft_strcpy(g_cmd->command, split[i]);
+	ft_strcpy(g_cmd->command, clean_command(split[i]));
 	i++;
 	if (split[i] && split[i][0] == '-')
 	{
