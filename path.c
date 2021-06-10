04c746e5c59e71f850ca10bdc6d71a382964a115
Salah eddine Mhah <smhah@e1r10p11.1337.ma>
2021-06-10 20:34:47 +0100

    makayn la final la walo

--- path.c
+++ path.c
@@ -6,7 +6,7 @@
 /*   By: smhah <smhah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 16:00:57 by cbenali-          #+#    #+#             */
-/*   Updated: 2021/06/10 17:55:18 by smhah            ###   ########.fr       */
+/*   Updated: 2021/06/10 20:31:59 by smhah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,9 @@ void	find_path(t_command_info *cmd, char *var, int p)
 	split = mod_split(search_path(), ':');
 	i = 0;
 	if (p)
+	{
 		execute_cmd(cmd, var);
+	}
 	while (split[i])
 	{
 		command = ft_strjoin(split[i], var);
